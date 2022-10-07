5ed46159f11c5a4bc987a888c053a79283135c14
PORELARTE <kamranbekirovyz@gmail.com>
2022-10-07 16:16:56 +0400

    Removed progress color property from AsanLoginConfig

--- lib/src/asan_login_config.dart
+++ lib/src/asan_login_config.dart
@@ -1,20 +1,15 @@
 import 'package:asan_login_flutter/src/asan_login_environment.dart';
-import 'package:flutter/material.dart';
 
 /// Configurations for [AsanLoginView]
 class AsanLoginConfig {
   /// Defines environment for `ASAN Login`.
   final AsanLoginEnvironment environment;
 
-  /// When set to true user stays as logged in if previously logged.
+  /// When set as true, user stays as logged in if previously logged.
   final bool clearCookies;
-
-  /// Color for progress indicator at the top of the [AsanLoginView].
-  final Color progressColor;
 
   const AsanLoginConfig({
     this.environment = AsanLoginEnvironment.prod,
     this.clearCookies = true,
-    this.progressColor = Colors.indigo,
   });
 }
